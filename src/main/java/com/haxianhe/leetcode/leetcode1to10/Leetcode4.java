ba5c10ec765eb6abf272218ff1ee2f9a971c15de
haxianhe <haxianhe@kuaishou.com>
2024-01-09 21:41:10 +0800

    20240109

---
+++ src/main/java/com/haxianhe/leetcode/leetcode1to10/Leetcode4.java
@@ -0,0 +1,11 @@
+package com.haxianhe.leetcode.leetcode1to10;
+
+/**
+ * @author haxianhe <haxianhe@kuaishou.com>
+ * Created on 2023-07-23
+ */
+public class Leetcode4 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/haxianhe/leetcode/leetcode1to10/Leetcode5.java
@@ -0,0 +1,17 @@
+package com.haxianhe.leetcode.leetcode1to10;
+
+/**
+ * @author haxianhe <haxianhe@kuaishou.com>
+ * Created on 2023-07-23
+ */
+public class Leetcode5 {
+    /**
+     * 给你一个字符串 s，找到 s 中最长的回文子串。
+     * 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+
+    }
+}
