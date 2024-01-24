1388a4c3432e7820dd1d8878fc05b7c232e80860
haxianhe <haxianhe@kuaishou.com>
2024-01-24 21:45:50 +0800

    [19]删除链表的倒数第N个节点

---
+++ src/main/java/com/haxianhe/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,83 @@
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+//
+// 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+//
+// Related Topics 链表 双指针 👍 2791 👎 0
+
+  
+package com.haxianhe.leetcode.editor.cn;
+
+import com.haxianhe.leetcode.jzoffer.ListNode;
+
+public class RemoveNthNodeFromEndOfList{
+  public static void main(String[] args) {
+       Solution solution = new RemoveNthNodeFromEndOfList().new Solution();
+  }
+  
+  //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/com/haxianhe/leetcode/jzoffer/ListNode.java
+++ src/main/java/com/haxianhe/leetcode/jzoffer/ListNode.java
@@ -9,10 +9,11 @@
 package com.haxianhe.leetcode.jzoffer;
 
 public class ListNode {
-    int val;
-    ListNode next = null;
-
-    ListNode(int val) {
+    public int val;
+    public ListNode next;
+    ListNode() {}
+    public ListNode(int val) {
         this.val = val;
     }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
