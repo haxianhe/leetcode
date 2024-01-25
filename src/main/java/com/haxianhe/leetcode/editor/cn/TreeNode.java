cb3ce09dbf49d33bbf1017ca318e503f1575b38e
haxianhe <haxianhe@kuaishou.com>
2024-01-25 08:45:31 +0800

    [94]二叉树的中序遍历

---
+++ src/main/java/com/haxianhe/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,34 @@
+package com.haxianhe.leetcode.editor.cn;
+
+/**
+ * @author haxianhe <haxianhe@kuaishou.com>
+ * Created on 2024-01-25
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
