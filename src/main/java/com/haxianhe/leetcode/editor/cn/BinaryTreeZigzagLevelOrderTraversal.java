08ed2b1db5139877db0cbd8c6a52887422de0b06
haxianhe <haxianhe@kuaishou.com>
2024-10-28 05:07:04 +0800

    [103]二叉树的锯齿形层序遍历

---
+++ src/main/java/com/haxianhe/leetcode/editor/cn/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,99 @@
+//给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[20,9],[15,7]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 923 👎 0
+
+  
+package com.haxianhe.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeZigzagLevelOrderTraversal{
+  public static void main(String[] args) {
+       Solution solution = new BinaryTreeZigzagLevelOrderTraversal().new Solution();
+  }
+  
+  //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        boolean leftToRight = true;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (leftToRight) {
+                    level.add(node.val);
+                } else {
+                    level.add(0, node.val);
+                }
+
+                if( node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(level);
+            leftToRight = !leftToRight;
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
